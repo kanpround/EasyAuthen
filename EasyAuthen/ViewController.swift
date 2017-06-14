c23e5326f2ddc26ec30fc974d05cc8bd111d7ac9
student09 <kannikar@snru.ac.th>
2017-06-14 10:30:48 +0700

    Check user OK

--- EasyAuthen/ViewController.swift
+++ EasyAuthen/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // Emplicit
+    var strUser: String?
+    var strPass: String?
+    
+    
     
     
     @IBOutlet weak var userTextField: UITextField!
@@ -21,22 +26,60 @@ class ViewController: UIViewController {
     
     
     @IBAction func loginButton(_ sender: Any) {
+       // get value form TextField
+        strUser = userTextField.text
+        strPass = passTextField.text
+        
+        // show user and pass on console
+        print("User ==> \(strUser!)")
+        print("Pass ==> \(strPass!)")
+        
+        // find count string
+        let  intUser = strUser?.characters.count
+        let  intPass = strUser?.characters.count
         
         
+        print("strUser ==> \(intUser!)")
+        print("strPass ==> \(intPass!)")
         
-    }
+        // call checkSpace
+        if checkSpace(myString: strUser!) {
+            print("User OK.")
+        } else{
+            print("Blank User!!!")
+        
+        }
+        
+    }   // end loginButton
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        
+    }   // end main method
 
+    func checkSpace(myString: String) -> Bool {
+        let intString = myString.characters.count
+        var result: Bool = true
+        
+        if intString == 0 {
+            // have space
+            result = false
+        }
+        
+        return result
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+        
+        
+    }   // end did recieve
 
     
 
-}
+} // end main class
 
